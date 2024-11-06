26edae7f4950530fa193303bf8440e5f0b49abf4
OmerM01 <omerwork00@gmail.com>
2024-11-06 14:31:44 +0200

    Assignment01

--- Ascend.java
+++ Ascend.java
@@ -6,7 +6,11 @@ public class Ascend {
 		double a = Math.random() * randMax;
 		double b = Math.random() * randMax;
 		double c = Math.random() * randMax;
+		double minNum = Math.min(Math.min(a , b) , c);
+		double maxNum = Math.max(Math.max(a , b) , c);
+		double midNum = (a + b + c) - minNum - maxNum;
+		System.out.println((int) a + " " + (int) b + " " +(int) c);
+		System.out.println((int) minNum + " " + (int) midNum + " " +(int) maxNum);
 
-		System.out.println(a + " " + b + " " + c);
 	}
 }
